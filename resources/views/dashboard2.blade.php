aa07a7fd1b918b096c960aee5d78eefe04135304
subhodeepmoitra <subhodeep2000@gmail.com>
2022-05-23 14:02:09 +0530

    user login and registration

---
+++ resources/views/dashboard2.blade.php
@@ -0,0 +1,99 @@
+
+<!doctype html>
+<html class="no-js" lang="">
+   <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>Dashboard Page</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="assets/css/normalize.css">
+      <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+      <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+      <link rel="stylesheet" href="assets/css/themify-icons.css">
+      <link rel="stylesheet" href="assets/css/pe-icon-7-filled.css">
+      <link rel="stylesheet" href="assets/css/flag-icon.min.css">
+      <link rel="stylesheet" href="assets/css/cs-skin-elastic.css">
+      <link rel="stylesheet" href="assets/css/style.css">
+      <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+   </head>
+   <body>
+      <aside id="left-panel" class="left-panel">
+         <nav class="navbar navbar-expand-sm navbar-default">
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+               <ul class="nav navbar-nav">
+                  <li class="menu-title">Menu</li>
+                  <li class="menu-item-has-children dropdown">
+                     <a href="categories.php" >Category Master</a>
+                  </li>
+                  <li class="menu-item-has-children dropdown">
+                     <a href="product.php" > Product Master</a>
+                  </li>
+				  <li class="menu-item-has-children dropdown">
+                     <a href="order.php" > Order Master</a>
+                  </li>
+
+				  <li class="menu-item-has-children dropdown">
+                     <a href="users.php" > User Master</a>
+                  </li>
+				  <li class="menu-item-has-children dropdown">
+                     <a href="contact_us.php" > Contact Us</a>
+                  </li>
+
+               </ul>
+            </div>
+         </nav>
+      </aside>
+      <div id="right-panel" class="right-panel">
+         <header id="header" class="header">
+            <div class="top-left">
+               <div class="navbar-header">
+                  <a class="navbar-brand" href="index.php"><img src="pic/icon.png" alt="logo3"></a>
+                  <a class="navbar-brand hidden" href="index.php"><img src="pic/icon.png" alt="logo3"></a>
+                  <a id="menuToggle" class="menutoggle"><i class="fa fa-bars"></i></a>
+               </div>
+            </div>
+            <div class="top-right">
+               <div class="header-menu">
+                  <div class="user-area dropdown float-right">
+                     <a href="#" class="dropdown-toggle active" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Welcome Admin</a>
+                     <div class="user-menu dropdown-menu">
+                        <a class="nav-link" href="{{ route('logout') }}"><i class="fa fa-power-off"></i>logout</a>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </header>
+<div class="content pb-0">
+	<div class="orders">
+	   <div class="row">
+		  <div class="col-xl-12">
+			 <div class="card">
+				<div class="card-body">
+				   <h4 class="box-title">Dashboard </h4>
+				</div>
+			</div>
+		  </div>
+	   </div>
+	</div>
+</div>
+
+<div class="clearfix"></div>
+         <footer class="site-footer">
+            <div class="footer-inner bg-white">
+               <div class="row">
+                  <div class="col-sm-6">
+                     Copyright &copy; <?php echo date('Y')?> TinyTressers
+                  </div>
+
+               </div>
+            </div>
+         </footer>
+      </div>
+      <script src="assets/js/vendor/jquery-2.1.4.min.js" type="text/javascript"></script>
+      <script src="assets/js/popper.min.js" type="text/javascript"></script>
+      <script src="assets/js/plugins.js" type="text/javascript"></script>
+      <script src="assets/js/main.js" type="text/javascript"></script>
+   </body>
+</html>
+
---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('logout', function ()
+{
+    Auth::logout();
+    session_unset();
+    return redirect(property_exists($this, 'redirectAfterLogout') ? $this->redirectAfterLogout : '/');
+
+    //these attempts will not remove values from the session.....
+
+    //session()->forget('db');
+    //\Session::flush();
+
+
+})->name('logout');
